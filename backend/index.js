5d8efe16c03704286ce02cb955fa15fb210b2644
PRABHUDDA <79395762+PRABHUDDA@users.noreply.github.com>
2023-10-01 11:50:13 +0530

    routes update

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 const PORT = process.env.PORT || 5001;
