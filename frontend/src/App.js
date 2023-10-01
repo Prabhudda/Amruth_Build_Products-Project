bef2d4ac785620cfc317691fef4bc4ba3bfb3783
PRABHUDDA <79395762+PRABHUDDA@users.noreply.github.com>
2023-10-01 11:29:38 +0530

    router update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Pages/Home_Page/Home';
 import About from './Pages/About/About';
